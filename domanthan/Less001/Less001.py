137af1d28a049bfc0c8fc4840d5a6799b1e65abc
domanthan <domanthan@gmail.com>
2018-08-31 00:57:29 +0900

    宿題１,2

---
+++ domanthan/Less001/Less001.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# What we learn here: How to test if you setups is finished.
+
+print ("Hello Python world!")
---
+++ domanthan/Less002/Less002.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# What we learn here: How to print, for loop controls.
+
+for i in range(1,101):
+    print("お母さん、愛している！")
