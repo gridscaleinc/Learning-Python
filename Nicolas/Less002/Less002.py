386243d1364f7b71257ab2bfe13cf2952f655edc
nicolas-lixiaodong <42858207+nicolas-lixiaodong@users.noreply.github.com>
2018-09-04 18:44:22 +0900

    commit
    Less002 hw

---
+++ Nicolas/Less002/Less002.py
@@ -0,0 +1,13 @@
+#Lesson 2  How to print, for loop controls.
+
+print("Import things need to be repeated three times.")
+for i in range(1,3):
+    print("Show me the money！ ")
+
+
+#Memo
+'''
+    1. for loop formart
+     - need the colon[:] at end the of for loop defination
+     - instead of {}  , use indent to control the for loop block   
+'''
