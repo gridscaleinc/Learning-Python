4d41327212d9408bf2e620ba251aac1573d8a1a5
domanthan <domanthan@gmail.com>
2018-09-05 18:23:27 +0900

    郵便番号補助

--- dongri/Less014/Less014.py
+++ dongri/Less014/Less014.py
@@ -5,7 +5,7 @@ from numpy import array
 
 girlSpeed = 3
 girlPosition = array([10, 5000])
-girlMove = array([1, 0.5])
+girlMove = array([1, -0.5])
 
 mySpeed = 1000
 myPosition = array([-1000, -1000])
@@ -15,7 +15,7 @@ flag = False
 for mySpeed in range(1000):
   time = 1
   while True:
-    girlStop = girlPosition - girlMove * girlSpeed * time
+    girlStop = girlPosition + girlMove * girlSpeed * time
     myStop = myPosition + myMove * mySpeed * time
     print(girlStop)
     print(myStop)
