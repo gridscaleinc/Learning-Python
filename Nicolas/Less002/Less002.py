46449ce3cc4d3e530dcfe0530e1d1116e4fe109c
nicolas-lixiaodong <42858207+nicolas-lixiaodong@users.noreply.github.com>
2018-09-06 16:22:45 +0900

    commit

--- Nicolas/Less002/Less002.py
+++ Nicolas/Less002/Less002.py
@@ -2,12 +2,32 @@
 
 print("Import things need to be repeated three times.")
 for i in range(1,3):
+    print (i)
     print("Show me the money！ ")
+
+print ("------------------")
+
+for i in range(3):
+    print (i)
+    print("Show me the money！ ")
+
+print ("------------------")
+
+for i in range(0,3):
+    print (i)
+    print("Show me the money！ ")
+
 
 
 #Memo
 '''
     1. for loop formart
      - need the colon[:] at end the of for loop defination
-     - instead of {}  , use indent to control the for loop block   
+     - instead of {}  , use indent to control the for loop block  
+     - keyword  “in” 
+0906
+    confirmed the for Loop range 
+    range （1 ，3） start  1
+    range （0,3） start  0 (default)
+    range （3） start  0 (default)
 '''
