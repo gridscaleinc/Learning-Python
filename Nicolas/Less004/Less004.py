84c9b763bc95735345fd1fc533ef40502c383a75
nicolas-lixiaodong <42858207+nicolas-lixiaodong@users.noreply.github.com>
2018-10-01 17:48:00 +0900

    commit

--- Nicolas/Less004/Less004.py
+++ Nicolas/Less004/Less004.py
@@ -7,6 +7,15 @@
 愛犬    ドフィー    2才
 '''
 
+iFamily = ['本人','ドム' ,'45才']
+iFamily.append('長女')
+iFamily.append('チェルシー')
+iFamily.append('12才')
+iFamily += ['愛犬','ドフィー','2才']
+
+for Famileyinfo in iFamily:
+    print(Famileyinfo)
+
 # Print exercises
 from datetime import date
 print()
@@ -20,3 +29,17 @@ print ("愛犬" + "   " + "ドフィー" + "   "  + "2才")
 
 
 
+
+
+
+
+''' Memo
+１．Print句相関
+      （１）.Python3の場合、Printが「Print文」から「Print関数」へと変わり、
+      それにあわせて、末尾に追加される文字を　end optionで指定できるようになりました。
+
+      （２）. from 。。。import。。。。の形で一文を宣言すると、print文を上書きする形で　Python３のPrint関数を
+      使うことができるようになります。
+
+２．Listの使い方
+'''
