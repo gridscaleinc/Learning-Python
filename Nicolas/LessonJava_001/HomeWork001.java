30ad8a5fef5c3cc93417b70eadc1638c4a0aa073
nicolas-lixiaodong <42858207+nicolas-lixiaodong@users.noreply.github.com>
2018-10-02 15:05:24 +0900

    commit

--- Nicolas/LessonJava_001/HomeWork001.java
+++ Nicolas/LessonJava_001/HomeWork001.java
@@ -1,17 +1,18 @@
+//20181002 modify source format
+
 package iHomeWork_Huang;
 
 public class HomeWork001 {
 	public static void main(String arg[]) {
 		int calc = 0;
-		int subtotal =0;
-		String pulsResult = "";
-		for (int i = 1;i<=1000;i++) {
+		int subtotal = 0;
+		String plusResult = "";
+		for (int i = 1; i <= 1000; i++) {
 			subtotal = calc + i;
-			pulsResult =  calc + "+" + i + "=" +  subtotal;
-			System.out.println(pulsResult);		//ouput the formula of every step;
+			plusResult = calc + "+" + i + "=" + subtotal;
+			System.out.println(plusResult);		//ouput the formula of every step;
 			calc = subtotal;
 		}
-
-		System.out.print("1+2+3+...1000= " + calc);
+		System.out.print("1+2+3+...+1000 = " + calc);
 	}
 }
