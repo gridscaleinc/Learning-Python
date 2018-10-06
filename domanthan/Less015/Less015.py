92958536ca1eebc14c382d221632d74198f74f62
domanthan <domanthan@gmail.com>
2018-10-06 23:26:58 +0900

    ちょっと更新

--- domanthan/Less015/Less015.py
+++ domanthan/Less015/Less015.py
@@ -3,6 +3,8 @@
 
 class Circle:
     radius = 0
+
+    # コンストラクタ
     def __init__(self, radius):
         self.radius = radius
 
@@ -12,24 +14,31 @@ class Circle:
 class Planet:
     revolutionPeriod = 0
 
+    # コンストラクタ
     def __init__(self, revolutionPeriod):
         self.revolutionPeriod = revolutionPeriod
 
+## 多重継承を注目
 class Globe(Circle, Planet):
     rotationPeriod = 24*60*60
     myRadius = 6371
 
+    # コンストラクタ
     def __init__(self):
         p = 365*24*60*60
+
+        ## 親クラスのコンストラクタの呼び出し方を注目。
         Circle.__init__(self, 149600000)
         Planet.__init__(self, p)
 
+    # 自転速度。
     def rotationSpeed(self):
         globeCircle = Circle(6371)
-        return round(globeCircle.circumLength() / self.rotationPeriod,2)
+        return round(globeCircle.circumLength() / self.rotationPeriod, 2)
 
+    # 公転速度
     def revolutionSpeed(self):
-        return round(self.circumLength() / self.revolutionPeriod,2)
+        return round(self.circumLength() / self.revolutionPeriod, 2)
 
 
 globe = Globe()
